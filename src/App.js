014426c4189f4d2fb1672e24a6845844784f1ecc
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-07 21:09:13 -0400

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from './components/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>DYLAN GALBRAITH</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import '../styles/Home.scss';
+
+function Home() {
+  return (
+    <main>
+      <h1>DYLAN GALBRAITH</h1>
+    </main>
+  )
+}
+
+export default Home;
