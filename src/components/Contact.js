9e3d3fde732db275f9fc6db4ca805acdff2a8510
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-18 13:51:51 -0400

    Update Contact logos to be white

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,14 @@
 import '../styles/Contact.scss';
 import gitHubLogo from '../assets/logos/github-logo.svg';
-import gmailLogo from '../assets/logos/gmail-logo.png';
-import linkedinLogo from '../assets/logos/linkedin-logo.png';
+import gmailLogo from '../assets/logos/gmail.svg';
+import linkedinLogo from '../assets/logos/linkedin.svg';
 
 function Contact() {
   return (
       <div id="contact" className="contact">
         <div className="contact__links">
           <a className="contact__link" href="https://github.com/dylan-galbraith"><img className="contact__logo" src={gitHubLogo} alt="github logo" /></a>
-          <a className="contact__link" href="mailto:dylangalbraith9@gmail.com"><img className="contact__logo" src={gmailLogo} alt="gamil logo" /></a>
+          <a className="contact__link" href="mailto:dylangalbraith9@gmail.com"><img className="contact__logo" src={gmailLogo} alt="email logo" /></a>
           <a className="contact__link" href="https://www.linkedin.com/in/dylan-galbraith/"><img className="contact__logo" src={linkedinLogo} alt="contact logo" /></a>
         </div>
       </div>
