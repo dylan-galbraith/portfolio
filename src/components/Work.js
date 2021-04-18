488c7fd0f6345b09d58a36548216b76e76440842
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-18 15:03:31 -0400

    Add transitions when expanding and collapsing work bios

--- src/components/Work.js
+++ src/components/Work.js
@@ -22,7 +22,8 @@ function Work() {
         <div>
           {fitTrack ? <p onClick={fitTrackDropDown} className="work__item__more work__item__more--right">Learn Less	&#8593;</p> : <p onClick={fitTrackDropDown} className="work__item__more work__item__more--right">Learn More &#8595;</p>}
         </div>
-        <div className={fitTrack ? "work__bio" : "hidden"} >
+      </div>
+      <div className={fitTrack ? "work__bio active" : "work__bio"} >
           <p className="work__item__info work__item__info--right">I developed this project while in school as my final assignment. Given 10 days, we had to come up with an idea for a web application that would solve some type of issue. I built FitTrack, a mobile application that allows you to create your own exercises and routines, and track your progress at the gym.</p>
           <p className="work__item__info work__item__info--right">To build this application, I used React and JavaScript, as well as CSS/SASS for my front-end. To set up authentication and authorization, I used Firebase. For the back-end, I used MySQL and Prisma to build the database, along with Express and Node.js for the server.</p>
           <div className="work__item__links">
@@ -30,7 +31,6 @@ function Work() {
             <a href="https://github.com/dylan-galbraith/fit-track" className="work__item__link work__item__link--right">https://github.com/dylan-galbraith/fit-track</a>
           </div>
         </div>
-      </div>
 
       <div className="work__item work__item--right">
         <p className="work__item__heading">ConnectMD</p>
@@ -38,14 +38,14 @@ function Work() {
         <div>
           {connectMD ? <p onClick={connectMDDropDown} className="work__item__more">Learn Less	&#8593;</p> : <p onClick={connectMDDropDown} className="work__item__more">Learn More &#8595;</p>}
         </div>
-        <div className={connectMD ? "work__bio" : "hidden"} >
+      </div>
+      <div className={connectMD ? "work__bio work__bio--right active" : "work__bio work__bio--right"} >
           <p className="work__item__info">This project was developed for a hackathon run by Google. Given <strong>24 hours</strong>, each team had to come up with a way to make health care more accessible to people through technology. Each team was made up of Developers, Designers, Digital Marketers, and Data Scientists. Our team came up with ConnectMD, a mobile application designed for those who's first language ins't English. The app would match each patient with a doctor who does speak their language, and can accomodate them for any religious/cultural/language preferences that were stopping them from seeing a doctor.</p>
           <p className="work__item__info">To build this application, we used React and SASS for the front-end. For the back-end, we made a mock server using Express and Node.js.</p>
           <div className="work__item__links">
             <a href="https://github.com/dylan-galbraith/tinder-md" className="work__item__link">https://github.com/dylan-galbraith/tinder-md</a>
           </div>
         </div>
-      </div>
 
     </div>
   )
