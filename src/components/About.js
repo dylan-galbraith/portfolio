688cc867e7a8c1af7e61abfdf2e24666db0f29fe
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-19 15:07:36 -0400

    Update About bio

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <div id="about" className="about">
         <div className="about__bio">
           <img className="about__headshot" src={headshot} alt="headshot" />
-          <p className="about__info">Hi there! My name's Dylan, and I am a full-stack web developer based in Toronto, Canada. Please, feel free to take a look at some of my past work, and contact me if you have any questions!</p>
+          <p className="about__info">Hello there, I'm Dylan, a full-stack web developer based out of Toronto, Canada. As a full-stack developer, I make sure all the websites I build are fast, dynamic, intuitive, and easy on the eyes. Keep scrolling to take a look at some of my past work, and feel free to get in touch with me <a className="about__link" href="mailto:dylangalbraith9@gmail.com">here</a> or through my links at the bottom!</p>
         </div>
       </div>
   )
