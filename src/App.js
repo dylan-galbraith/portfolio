9894f727fb07084794a182c590ce368a149c3fec
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-28 17:30:01 -0400

    Delete everything. Start over.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import About from './components/About';
-import Contact from './components/Contact';
-import Home from './components/Home';
-import Work from './components/Work';
 import './App.scss';
-import NavBar from './components/NavBar';
-
-const scrollToHome = (e) => {
-  document.getElementById("home").scrollIntoView();
-}
-
-const scrollToAbout = (e) => {
-  document.getElementById("about").scrollIntoView({block: "center"});
-}
-
-const scrollToWork = (e) => {
-  document.getElementById("work").scrollIntoView();
-}
-
-const scrollToContact = (e) => {
-  document.getElementById("contact").scrollIntoView();
-}
 
 function App() {
   return (
     <div className="App">
-      <NavBar home={scrollToHome} about={scrollToAbout} work={scrollToWork} contact={scrollToContact} />
-      <Home scrollDown={scrollToAbout} />
-      <About scrollUp={scrollToHome} scrollDown={scrollToWork} />
-      <Work scrollUp={scrollToAbout} scrollDown={scrollToContact} />
-      <Contact scrollUp={scrollToWork} />
     </div>
   );
 }
