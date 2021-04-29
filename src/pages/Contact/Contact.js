ed794903f9033e2abfe2a9c41ac56d8059e6f753
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 17:56:14 -0400

    Add file to fix 404 errors with netlify

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -14,8 +14,8 @@ export default function Contact() {
       </div>
       <form className="contact__form" name="contact" netlify method="POST">
         <input type="hidden" name="form-name" value="contact" />
-        <input className="contact__input" placeholder="first name" name="fName"/>
-        <input className="contact__input" placeholder="last name"name="lName" />
+        <input className="contact__input" placeholder="first name" name="firstName"/>
+        <input className="contact__input" placeholder="last name"name="lastName" />
         <input className="contact__input" placeholder="email address" name="email" />
         <textarea className="contact__input contact__input--textarea" placeholder="message" name="message" />
         <button className="contact__button" type="submit">SEND</button>
