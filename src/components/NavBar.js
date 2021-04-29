87f970ab3c7bdff75d1ddd0ebd0557cb8733ef21
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-28 22:03:48 -0400

    Hide home button on nav bar when on home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
-export default function NavBar() {
+export default function NavBar(props) {
+
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <nav className="nav">
-      <NavLink to='/' exact className="nav__link nav__link--home">dg</NavLink>
+      <NavLink to='/' exact className={location.pathname==="/" ? "hidden" : "nav__link nav__link--home"}>&#8592;</NavLink>
       <NavLink to='/about' className="nav__link">about</NavLink>
       <NavLink to='/work' className="nav__link">work</NavLink>
       <NavLink to='/contact' className="nav__link">contact</NavLink>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,8 +6,6 @@ export default function Home() {
   return (
     <main className="home">
       <img className="home__logo" src={logo} alt="" />
-      {/* <h1 className="home__heading">dylan<br/>galbraith</h1>
-      <p className="home__subheading">full stack web developer</p> */}
     </main>
   )
 }
