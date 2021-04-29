5c3a98a4e8e23c0298dc393b722434f6520c2669
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 17:39:39 -0400

    Add POST method to form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -12,7 +12,7 @@ export default function Contact() {
       <div className="contact__hero">
         <h1 className="contact__heading">contact<br/>me</h1>
       </div>
-      <form className="contact__form" name="contact" netlify>
+      <form className="contact__form" name="contact" netlify method="POST">
         <input className="contact__input" placeholder="first name" name="fName"/>
         <input className="contact__input" placeholder="last name"name="lName" />
         <input className="contact__input" placeholder="email address" name="email" />
