768be05f2a100357b02da90cdb1f76a61e74b82f
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 17:35:08 -0400

    Debug form functionality

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -12,12 +12,12 @@ export default function Contact() {
       <div className="contact__hero">
         <h1 className="contact__heading">contact<br/>me</h1>
       </div>
-      <form className="contact__form" name="contact" method="POST" data-netlify="true">
-        <input className="contact__input" placeholder="first name"/>
-        <input className="contact__input" placeholder="last name"/>
-        <input className="contact__input" placeholder="email address"/>
-        <textarea className="contact__input contact__input--textarea" placeholder="message"/>
-        <button className="contact__button">SEND</button>
+      <form className="contact__form" name="contact" netlify>
+        <input className="contact__input" placeholder="first name" name="fName"/>
+        <input className="contact__input" placeholder="last name"name="lName" />
+        <input className="contact__input" placeholder="email address" name="email" />
+        <textarea className="contact__input contact__input--textarea" placeholder="message" name="message" />
+        <button className="contact__button" type="submit">SEND</button>
       </form>
     </main>
   )
