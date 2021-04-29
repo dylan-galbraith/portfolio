dfb4cd35ea489e2383ebafe00339835557353e05
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 15:11:07 -0400

    Style nav bar for mobile devices

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
+import menu from '../assets/icons/menu.svg'
 
-export default function NavBar(props) {
+export default function NavBar() {
 
   const location = useLocation();
   console.log(location.pathname);
 
+  const [open, setOpen] = useState(true)
+  function openMenu() {
+    setOpen(!open)
+  }
+
   return (
-    <nav className="nav">
-      <NavLink to='/' exact className={location.pathname==="/" ? "hidden" : "nav__link nav__link--home"}>&#8592;</NavLink>
+    <nav className={location.pathname==="/" ? "nav" : "nav nav__background"}>
+      <NavLink to='/' exact className={location.pathname==="/" ? "hidden" : "nav__link nav__link--home"}>dylan<br/>galbraith</NavLink>
       <NavLink to='/about' className="nav__link">about</NavLink>
       <NavLink to='/work' className="nav__link">work</NavLink>
       <NavLink to='/contact' className="nav__link">contact</NavLink>
