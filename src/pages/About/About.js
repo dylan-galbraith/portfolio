8144eceb04b7e0632f856ebd09f42c9924b5f6c3
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 19:58:50 -0400

    Update styling on About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -13,8 +13,8 @@ export default function About() {
       <div className="about__hero">
         <h1 className="about__heading">about<br/>me</h1>
       </div>
-      <img className="about__image" src={heroImage} alt="" />
       <p className="about__text">Hi! My name’s Dylan Galbraith, and I'm a full stack web developer with a caffeine addiction based out of Toronto, Ontario. As a full stack developer, I pride myself on building robust web applications with a user-friendly and intuitive interface, paired with a fast and efficient back-end. </p>
+      <img className="about__image" src={heroImage} alt="" />
       <p className="about__text">Please feel free to take a look through my portfolio and past work, and contact me with any questions you may have!</p>
     </main>
   )
