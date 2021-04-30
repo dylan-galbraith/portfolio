c8b02801a580332f7c58ecad189c471d7419122a
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 21:46:07 -0400

    Change footer padding

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,9 +15,11 @@ export default function About() {
         <h1 className="about__heading">about<br/>me</h1>
         <img className="about__arrow" src={arrow} alt="" />
       </div>
-      <p className="about__text">Hi! My name’s Dylan Galbraith, and I'm a full stack web developer with a caffeine addiction based out of Toronto, Ontario. As a full stack developer, I pride myself on building robust web applications with a user-friendly and intuitive interface, paired with a fast and efficient back-end. </p>
-      <img className="about__image" src={heroImage} alt="" />
-      <p className="about__text">Please feel free to take a look through my portfolio and past work, and contact me with any questions you may have!</p>
+      <div className="about__bio">
+        <p className="about__text">Hi! My name’s Dylan Galbraith, and I'm a full stack web developer with a caffeine addiction based out of Toronto, Ontario. As a full stack developer, I pride myself on building robust web applications with a user-friendly and intuitive interface, paired with a fast and efficient back-end. </p>
+        <img className="about__image" src={heroImage} alt="" />
+        <p className="about__text">Please feel free to take a look through my portfolio and past work, and contact me with any questions you may have!</p>
+      </div>
     </main>
   )
 }
