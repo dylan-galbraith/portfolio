a187985cce86e7916db97e7e30026e2007a3ab7a
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-30 15:21:32 -0400

    Add modal to ConnectMD

--- src/pages/Work/Connectmd.js
+++ src/pages/Work/Connectmd.js
@@ -1,8 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Modal from 'react-modal';
 import image1 from '../../assets/images/connectmd/screenshot1.png'
 import image2 from '../../assets/images/connectmd/screenshot2.png'
-import image4 from '../../assets/images/connectmd/screenshot4.png'
+import image3 from '../../assets/images/connectmd/screenshot4.png'
 import arrow from '../../assets/icons/arrow.svg'
+
+Modal.setAppElement(document.getElementById('root'))
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.6)'
+  }
+}
 
 export default function Connectmd() {
 
@@ -10,8 +19,28 @@ export default function Connectmd() {
     window.scrollTo(0,0)
   }, [])
 
+  const [modalIsOpen,setIsOpen] = useState(false);
+  const [openImage, setOpenImage] = useState("")
+  function openModal(image) {
+    setIsOpen(true);
+    setOpenImage(image)
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
   return (
     <main className="project">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="project__modal"
+        style={customStyles}
+      >
+        <img className="project__modal__image" src={openImage} alt="" />
+        <button onClick={closeModal} className="project__modal__button">close</button>
+      </Modal>
       <div className="project__hero">
         <h1 className="project__heading">Connect<br/>MD</h1>
         <p className="project__tech">REACT | SASS | JAVASCRIPT | EXPRESS | NODE.JS</p>
@@ -20,9 +49,9 @@ export default function Connectmd() {
       <div className="project__bio">
         <p className="project__text">This project was developed for a hackathon sponsored by Google. Given 24 HOURS, each team had to come up with a way to make health care more accessible to people through technology. Each team was made up of Developers, Designers, Digital Marketers, and Data Scientists. Our team came up with ConnectMD, a mobile application designed for those who's first language ins't English. The app would match each patient with a doctor who does speak their language, and can accomodate them for any religious/cultural/language preferences that were stopping them from seeing a doctor.</p>
         <div className="project__images">
-          <img className="project__images__each" src={image1} alt="" />
-          <img className="project__images__each" src={image2} alt="" />
-          <img className="project__images__each" src={image4} alt="" />
+          <img onClick={()=>openModal(image1)} className="project__images__each" src={image1} alt="" />
+          <img onClick={()=>openModal(image2)} className="project__images__each" src={image2} alt="" />
+          <img onClick={()=>openModal(image3)} className="project__images__each" src={image3} alt="" />
         </div>
         <p className="project__text">To build this application, we used React and JavaScript, along with SASS for the front-end. For the back-end, we made a mock server using Express and Node.js. Our server is functional, but for the hackathon, we didn't feel it necessary to connect it to the front-end.</p>
         <div className="project__cta">
--- src/pages/Work/Fittrack.js
+++ src/pages/Work/Fittrack.js
@@ -9,7 +9,6 @@ Modal.setAppElement(document.getElementById('root'))
 
 const customStyles = {
   overlay: {
-    backgroundColor: 'rgba(255,255,255,0.3)',
     backgroundColor: 'rgba(0,0,0,0.6)'
   }
 }
