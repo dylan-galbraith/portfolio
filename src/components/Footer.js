fc7be52173732ddbc711779c30ed1484e9d674ce
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-29 22:09:07 -0400

    hide footer on home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useLocation } from 'react-router';
 import github from '../assets/icons/github.svg'
 import gmail from '../assets/icons/gmail.svg'
 import linkedin from '../assets/icons/linkedin.svg'
 
 export default function Footer() {
+
+  const location = useLocation();
+
   return (
-    <footer className="footer">
+    <footer className={location.pathname === "/" ? "hidden" : "footer"}>
       <a href="https://github.com/dylan-galbraith" target="_blank" rel="noopener noreferrer"><img className="footer__icon" src={github} alt="" /></a>
       <a href="mailto:dylangalbraith9@gmail.com"><img className="footer__icon" src={gmail} alt="" /></a>
       <a href="https://www.linkedin.com/in/dylan-galbraith/" target="_blank" rel="noopener noreferrer"><img className="footer__icon" src={linkedin} alt="" /></a>
